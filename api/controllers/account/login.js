d4b79ee0890c74a1b8c2fbbf53495d17868b7d1c
John Choi <officialjonguk@gmail.com>
2019-11-17 01:38:03 -0800

    no message

--- api/controllers/account/login.js
+++ api/controllers/account/login.js
@@ -2,12 +2,12 @@ const knex = require('../../../database');
 const bcrypt = require('bcrypt');
 
 const login = async (req, res) => {
-    console.log(req.body);
-
     const email = req.body.email;
     const password = req.body.password;
 
     const accRes = await knex('accounts').select().where({ email: email }).limit(1);
+
+    console.log(accRes[0].password);
 
     const valid = bcrypt.compareSync(password, accRes[0].password);
 
