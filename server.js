f6d3fa4192f39d6a590504d77ebb49d95a134ef9
John Choi <officialjonguk@gmail.com>
2019-11-17 01:15:27 -0800

    no message

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-//app.use(cors());
 app.set('json spaces', 4);
 
 app.use('/account', require('./api/routes/account'));
