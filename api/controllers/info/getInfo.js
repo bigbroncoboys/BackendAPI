7edeb56b62f1b726e55a69becbd665b656dc03b6
John Choi <officialjonguk@gmail.com>
2019-11-17 04:23:14 -0800

    no message

--- api/controllers/info/getInfo.js
+++ api/controllers/info/getInfo.js
@@ -4,20 +4,12 @@ const getInfo = async (req, res) => {
     const accountID = req.params.accountID;
 
     const infoRes = await knex('business_info').select().where({ account_id: accountID }).limit(1);
-    const emplRes = await knex('employees').select('name').where({ account_id: accountID });
 
-    const accountInfo = {
+    res.json({
         businessName: infoRes[0].name,
         businessAddress: infoRes[0].address,
         businessType: infoRes[0].type,
-        employees: []
-    }
-
-    emplRes.forEach(employee => {
-        accountInfo.employees.push(employee.name);
     });
-
-    res.json(accountInfo);
 }
 
 module.exports = getInfo
