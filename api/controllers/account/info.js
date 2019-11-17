be1a9394aaa579cf799e6c3394f83d68e128c459
John Choi <officialjonguk@gmail.com>
2019-11-17 01:54:06 -0800

    no message

---
+++ api/controllers/account/info.js
@@ -0,0 +1,23 @@
+const knex = require('../../../database');
+
+const info = async (req, res) => {
+    const accountID = req.body.accountID;
+
+    const infoRes = await knex('business_info').select().where({ account_id: accountID }).limit(1);
+    const emplRes = await knex('employees').select('name').where({ account_id: accountID });
+
+    const accountInfo = {
+        businessName: infoRes[0].name,
+        businessAddress: infoRes[0].address,
+        businessType: infoRes[0].type,
+        employees: []
+    }
+
+    emplRes.forEach(employee => {
+        accountInfo.employees.push(employee.name);
+    });
+
+    res.json(accountInfo);
+}
+
+module.exports = info
