6b32e8bc4ab0b62f1736be5748e580c48bd801d9
John Choi <officialjonguk@gmail.com>
2019-11-20 08:52:01 -0800

    no message

--- api/controllers/charge/checkPayment.js
+++ api/controllers/charge/checkPayment.js
@@ -1,15 +1,9 @@
 const knex = require('../../../database');
 
 const checkPayment = async (req, res) => {
-    console.log(req.query);
-    console.log(req.params);
-
     const sessionID = req.query.session_id;
 
     const dbRes = await knex('payment_sessions').select('processed').where({ session_id: sessionID }).limit(1);
-
-    console.log(dbRes);
-    console.log('Requested ' + Date.now());
 
     res.json({ processed: dbRes[0].processed });
 }
